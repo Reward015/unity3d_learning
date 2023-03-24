c77e051613e04fb7d3775fd14180f9c91fa29e28
Duy Ha <trongduy196@gmail.com>
2023-03-24 23:10:16 +0700

    git add *!

--- Project Boost/Assets/Scripts/Movement.cs
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -4,23 +4,40 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] float mainThrust = 100f;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         ProcessInput();
+        ProcessRotation();
     }
 
     void ProcessInput()
     {
         if (Input.GetKey (KeyCode.Space))
         {
-            Debug.Log("Pressed SPACE - Thrusting");
+            rb.AddRelativeForce(Vector3.up * mainThrust *  Time.deltaTime);  
+        }
+        
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * mainThrust *  Time.deltaTime);
+        }
+    
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * mainThrust *  Time.deltaTime);
         }
     }
 }
