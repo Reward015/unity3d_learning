0281c20af4247604e2f798e74f52476186bd3378
Duy Ha <trongduy196@gmail.com>
2023-03-29 22:45:46 +0700

    git add *

--- Project Boost/Assets/Scripts/Movement.cs
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -5,25 +5,32 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rotationThrust = 1f;
     Rigidbody rb;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        ProcessThrust();
         ProcessRotation();
     }
 
-    void ProcessInput()
+    void ProcessThrust()
     {
         if (Input.GetKey (KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up * mainThrust *  Time.deltaTime);  
+            if(!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
         
     }
@@ -32,12 +39,18 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward * mainThrust *  Time.deltaTime);
+            ApplyRotation(rotationThrust);
         }
     
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward * mainThrust *  Time.deltaTime);
+            ApplyRotation(-rotationThrust);
         }
+    }
+    public void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationThisFrame *  Time.deltaTime);
+        rb.freezeRotation = false;
     }
 }
