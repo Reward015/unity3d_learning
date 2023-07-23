8d6cd4363cbd4b4bd51ff59a3b6dd5e3aeeb0c9c
Reward015 <127296871+Reward015@users.noreply.github.com>
2023-07-23 23:43:41 +0700

    .

---
+++ ZombieRunner/Assets/EnemyAI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent navMeshAgent;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
